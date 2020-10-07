e16c95b708dd9484cd7ad8c578f29769939015d7
amiedawn <amie.d.carroll@gmail.com>
2020-10-07 00:16:45 -0600

    convert string to integer

--- assets/js/game.js
+++ assets/js/game.js
@@ -153,23 +153,23 @@ var endGame = function() {
 var shop = function() {
   // ask player what they would like to do
   var shopOptionPrompt = window.prompt(
-    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."  
+    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."  
   );
 
-  //use switch to carry out action
+  // converts string from window.prompt to an integer that will work in the upcoming switch
+  shopOptionPrompt = parseInt(shopOptionPrompt); 
+
+  //use switch to carry out action where 1 is refill, 2 is upgrade, 3 is leave
   switch (shopOptionPrompt) {
-    case "REFILL": // new case
-    case "refill":
+    case 1:
       playerInfo.refillHealth();
       break;
 
-    case "UPGRADE": // new case
-    case "upgrade":
+    case 2:
       playerInfo.upgradeAttack();
       break;
 
-    case "LEAVE": // new case
-    case "leave":
+    case 3:
       window.alert("Leaving the store.");
 
       // do nothing, so function will end
